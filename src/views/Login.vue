0c6f287180ca3591883a4b9a26d9a5e749e53c0c
zwf193071 <997131679@qq.com>
2019-09-29 13:45:37 +0800

    引入iview，完善登陆页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -22,24 +22,94 @@
 				clearable
 				ref="password"
 				class="input-item"
-				style="margin-bottom: 36px"
+				@on-enter="login"
+				style="margin-bottom: 10px;"
 			/>
-            <Button class="login-btn" type="default" long size="large" aria-label="登录"></Button>
+			<div class="checkbox" style="margin-bottom: 12px; color: #fff">
+				<Checkbox v-model="remmberPw">记住密码</Checkbox>
+				<label class="forget" style="float: right;padding-right: 1px;">忘记密码?</label>
+			</div>
+            <Button class="login-btn" type="default" long size="large" aria-label="登录" @click="login"></Button>
 		</div>
 		<div class="footer">@2019 ALL RIGHT RECEIVED 中博空间 版权所有</div>
 	</div>
 </template>
 <script lang="ts">
-import { Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { apiCover } from '@/api/index'
+
+@Component
 export default class Login extends Vue {
 	private account: string = '';
-	private password: string = '';
+  	private password: string = '';
+	private remmberPw: string = '';
 
-	toPassword() {
-		console.log(this)
+  	public toPassword() {
+    	this.$refs.account.blur();
+    	this.$refs.password.focus();
+  	}
+  	public checkLogin() {
+		const isValueNull = () => {
+	  		if(this.account) {
+        		if(this.password) {
+           			return true;
+        		} else {
+          			return 'noPassword';
+        		}
+      		} else {
+        		return 'noAccount';
+      		}
+		};
+		return new Promise((resolve, reject) => {
+			if (isValueNull() === true) {
+				resolve();
+			} else {
+				reject(isValueNull());
+			}
+		})
+  	}
+  	public login() {
+	  	this.checkLogin()
+		.then(() => {
+			const data = {
+				username: this.account,
+				password: this.password
+			}
+			apiCover({
+				url: 'Login',
+				data
+			})
+			.then( (res: object) => {
+			})
+			.catch( (error: object) => {
+				this.$Notice.error({
+					title: '登陆提示',
+					desc: '登陆失败！'
+				})
+			})
+		})
+		.catch((err: string) => {
+			const msg: string = err =='noAccount'?'用户名为空！':'密码为空！'
+			if(!this.password){
+				this.$Notice.error({
+					title: '登陆提示',
+					desc: msg
+				})
+			}
+		})
 	}
 }
 </script>
+<style>
+.login-box .ivu-input-large{
+	margin-bottom: 15px;
+	height: 40px;
+	width:350px;
+	background-color: rgba(36,39,45,0.6);
+	border: none;
+	font-size: 1.5625rem;
+}
+</style>
 <style scoped>
 .login{
 	width:100%;
@@ -57,14 +127,6 @@ export default class Login extends Vue {
 	bottom: 5%;
 	left: 23%;
 }
-.login-box .ivu-input-large{
-	margin-bottom: 15px;
-	height: 40px;
-	width:350px;
-	background-color: rgba(36,39,45,0.6);
-	border: none;
-	font-size: 1.5625rem;
-}
 .login-box .info{
   	color: #FFF;
 	font-size: 1.5rem;
@@ -78,8 +140,9 @@ export default class Login extends Vue {
 .login-box .login-btn{
 	height: 40px;
 	width:350px;
-	background: #000000 url('../assets/login/submit.png') no-repeat center center;
+	background: #000 url('../assets/login/submit.png') no-repeat center center;
 	background-size: auto 40px ;
+	border: 1px solid #000;
 	color: #FFF;
 }
 .footer{
